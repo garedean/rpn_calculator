0387a2c9ebb75947baa5f507681800d9672ff329
garedean <garrettdolson@gmail.com>
2016-05-11 20:05:32 -0700

    Add app.rb and Rake task for receiving user input

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require_relative 'app'
+
+namespace :app do
+  task :run do
+    App.new.run
+  end
+end
--- app.rb
+++ app.rb
@@ -1,1 +1,28 @@
+#!/usr/bin/env ruby
 
+require_relative 'lib/rpn_calculator'
+
+class App
+  def initialize(rpn_calculator: RpnCalculator.new)
+    @rpn_calculator = rpn_calculator
+  end
+
+  def run
+    print_directions
+
+    loop do
+      user_input = STDIN.gets.chomp
+
+      puts @rpn_calculator.evaluate_char(user_input)
+      exit if user_input == 'q'
+    end
+  end
+
+  private
+
+  def print_directions
+    puts 'Welcome! This is a Reverse Polish Calculator. '\
+         'Starting typing characters to begin evaluating '\
+         'input.'
+  end
+end
