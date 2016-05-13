4dc248ff587a7c415a3a11c0e65fd655436dcbf4
garedean <garrettdolson@gmail.com>
2016-05-12 22:06:20 -0700

    Add spec testing that entering 'q' exits program

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,23 +4,31 @@ require_relative '../app'
 
 describe App do
   describe '#run' do
-    before(:each) do
-      rpn_calculator = double('Calculator', evaluate_char: '1')
+    def build_app(simulate_user_input_with:)
+      rpn_calculator = double(
+        'Calculator', evaluate_char: simulate_user_input_with)
 
-      @app = App.new(rpn_calculator: rpn_calculator, logger: nil)
+      App.new(rpn_calculator: rpn_calculator, logger: nil)
     end
 
-    before do
-      @io_object = double('IO Object')
-      expect(@app).to receive(:gets).and_return(@io_object)
+    let(:io_object) do
+      double('IO Object')
     end
 
     it 'should display output from rpn_calculator' do
-      expect(@io_object).to receive(:chomp).and_return('1')
+      app = build_app(simulate_user_input_with: '1')
+      expect(app).to receive(:gets).and_return(io_object)
+      expect(io_object).to receive(:chomp).and_return('1')
 
-      rpn_calculator = double('Calculator', evaluate_char: '1')
+      expect { app.run }.to output("1\n").to_stdout
+    end
 
-      expect { @app.run }.to output("1\n").to_stdout
+    it "should exit the program if 'q' is received" do
+      app = build_app(simulate_user_input_with: 'q')
+      expect(app).to receive(:gets).and_return(io_object)
+      expect(io_object).to receive(:chomp).and_return('q')
+
+      expect { app.run }.to raise_error(SystemExit)
     end
   end
 end
